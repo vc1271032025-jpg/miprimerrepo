d277afe321a0fc7a30de23eb43cfdc996c362786
vc1271032025 <vc127032025@unab.edu.sv>
2026-04-08 00:19:59 -0600

    aca si empieza la guia y este es el primero ojo todos estan los codigos virgenes (como me proyecte e jlfvkdjfnvkjef) por asi decirlo es el mero codigo ahi falta añadir cosas si lo quiere testear

---
+++ labo2/ejercicio1.py
@@ -0,0 +1,9 @@
+def transformar_texto(texto, opcion):
+    if opcion == 1:
+        return texto.upper()
+    elif opcion == 2:
+        return texto.lower()
+    elif opcion == 3:
+        return texto.capitalize()
+
+    return texto
