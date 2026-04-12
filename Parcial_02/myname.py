ee12c6245670a9804e25abecfb3dd6403be7badb
vc1271032025 <vc127032025@unab.edu.sv>
2026-04-12 01:03:46 -0600

    Parcial 02 Fundamentos de programación, ejercicio 2 se remplaza el nombre por el apellido con replace y se pone la inicial en mayuscula con title

---
+++ Parcial_02/myname.py
@@ -0,0 +1,8 @@
+texto_original = "cristian arie"
+
+texto_titulado = texto_original.title()
+
+resultado_final = texto_titulado.replace("Cristian Ariel", "valle claros")
+
+final_titulado = resultado_final.title()
+print(resultado_final)
