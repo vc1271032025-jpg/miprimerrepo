f3e190c881281e705d5a22eed9b4885f98341ac2
vc1271032025 <vc127032025@unab.edu.sv>
2026-05-01 01:57:28 -0600

    sigo arreglando mi regada

--- labo2/ejercicio1.py
+++
@@ -1,9 +0,0 @@
-def transformar_texto(texto, opcion):
-    if opcion == 1:
-        return texto.upper()
-    elif opcion == 2:
-        return texto.lower()
-    elif opcion == 3:
-        return texto.capitalize()
-
-    return texto
--- labo2/ejercicio7.py
+++
@@ -1,9 +0,0 @@
-def transformaciones_secuenciales(texto, lista_numeros):
-    for num in lista_numeros:
-        if num == 1:
-            texto = texto.upper()
-        elif num == 2:
-            texto = texto.lower()
-        elif num == 3:
-            texto = texto.capitalize()
-    return texto
